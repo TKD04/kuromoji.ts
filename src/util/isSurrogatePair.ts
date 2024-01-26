d3bdc8b56c5c1c52b6dedb1f82af414d8e50d06b
TKD04 <tkd04fordev@outlook.com>
2024-01-26 19:10:20 +0900

    Add `number` to type of arguement `char`

--- src/util/isSurrogatePair.ts
+++ src/util/isSurrogatePair.ts
@@ -1,5 +1,6 @@
-export default (char: string): boolean => {
-  const utf16Code = char.charCodeAt(0);
+export default (char: string | number): boolean => {
+  const isString = typeof char === "string";
+  const utf16Code = isString ? char.charCodeAt(0) : char;
 
   return utf16Code >= 0xd800 && utf16Code <= 0xdbff;
 };
