2fcddb05f5ac2f4e8a5f9cbb8abf79a090a31be4
TKD04 <tkd04fordev@outlook.com>
2024-12-23 23:10:45 +0900

    Remove "ForOfStatement" from eslint-config-airnbnb to avoid conflict with `unicorn/no-array-for-each`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -92,6 +92,32 @@ export default tseslint.config(
           ],
         },
       ],
+      // Remove "ForOfStatement" from eslint-config-airnbnb to avoid conflict with "unicorn/no-array-for-each"
+      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v56.0.1/docs/rules/no-array-for-each.md
+      // https://github.com/airbnb/javascript/issues/1271
+      "no-restricted-syntax": [
+        "error",
+        {
+          message:
+            "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+          selector: "ForInStatement",
+        },
+        // {
+        //   message:
+        //     "iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.",
+        //   selector: "ForOfStatement",
+        // },
+        {
+          message:
+            "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+          selector: "LabeledStatement",
+        },
+        {
+          message:
+            "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+          selector: "WithStatement",
+        },
+      ],
       // Disable perfectionist/sort-imports to avoid conflicts with eslint-plugin-simple-import-sort
       "perfectionist/sort-imports": "off",
       "simple-import-sort/exports": "error",
