bf59fc25cfc1443928e14dd2b3cd9a3b28ab4b17
TKD04 <tkd04fordev@outlook.com>
2024-12-28 15:58:30 +0900

    Fix `perfectionist/sort-objects`

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   coverageProvider: "v8",
+  preset: "ts-jest",
   roots: ["<rootDir>/src"],
   testEnvironment: "node",
-  preset: "ts-jest",
 };
