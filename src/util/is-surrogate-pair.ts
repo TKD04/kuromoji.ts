c2abd176a1e6f7978e8c9bcd8eb77733affcc0c8
TKD04 <tkd04fordev@outlook.com>
2025-01-06 20:49:52 +0900

    Fix `isSurrogatePair()` returns `false` for low surrogate code point

--- src/util/is-surrogate-pair.ts
+++ src/util/is-surrogate-pair.ts
@@ -1,9 +1,12 @@
+import isHighSurrogate from "./is-high-surrogate";
+import isLowSurrogate from "./is-low-surrogate";
+
 /**
  * Returns a boolean value representing whether the codePoint is a high or low surrogate.
  * @param codePoint A non-negative integer number representing the code point value of the UTF-16 encoded code point.
  * @returns A boolean value representing whether the codePoint is a high or low surrogate.
  */
 const isSurrogatePair = (codePoint: number): boolean =>
-  codePoint >= 0xd8_00 && codePoint <= 0xdb_ff;
+  isHighSurrogate(codePoint) || isLowSurrogate(codePoint);
 
 export default isSurrogatePair;
